7f54a8d4e1ed97c4f96a61f2fb79ae976b52a64b
astarrrrrrrrrr-wq <astarrrrrrrrrr@gmail.com>
2025-07-23 20:05:28 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,23 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from threading import Thread
 
 class WebhookHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(b"Bot is alive!")
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Post request received")
 
 def run_http_server():
     port = int(os.environ.get("PORT", 8080))
@@ -15,6 +25,5 @@ def run_http_server():
     print(f"✅ HTTP server running on port {port}")
     server.serve_forever()
 
-def keep_alive():
-    t = Thread(target=run_http_server)
-    t.start()
+if __name__ == "__main__":
+    run_http_server()
